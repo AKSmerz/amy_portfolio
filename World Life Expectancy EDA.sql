4cb141ae9bb4114ddfeaf292f2c40d500ab8b6e1
AKSmerz <amyksmerz@gmail.com>
2025-09-25 10:55:09 -0500

    Add files via upload

---
+++ World Life Expectancy EDA.sql
@@ -0,0 +1,82 @@
+
+#World Life Expectancy (EXPLORATORY DATA ANALYSIS)
+
+#Starting Code
+SELECT *
+FROM world_life_expectancy
+;
+
+
+SELECT Country, 
+MIN(`Life Expectancy`), 
+MAX(`Life Expectancy`),
+ROUND(MAX(`Life Expectancy`)-MIN(`Life Expectancy`),1) AS Life_Increase_15_Years
+FROM world_life_expectancy
+GROUP BY Country
+HAVING MIN(`Life Expectancy`) <> 0
+AND MAX(`Life Expectancy`) <> 0
+ORDER BY Life_Increase_15_Years asc
+;
+#largest jump in life expectancy
+
+
+SELECT Year, ROUND(AVG(`Life Expectancy`),2)
+FROM world_life_expectancy
+WHERE `Life Expectancy`<> 0
+GROUP BY Year
+ORDER BY Year
+;
+
+SELECT *
+FROM world_life_expectancy
+;
+
+SELECT Country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(GDP),1) AS GDP
+FROM world_life_expectancy
+GROUP BY Country
+HAVING Life_Exp > 0
+AND GDP > 0
+ORDER BY GDP DESC
+;
+
+
+SELECT 
+SUM(CASE WHEN GDP >= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
+AVG(CASE WHEN GDP >= 1500 THEN `Life Expectancy` ELSE NULL END) High_GDP_Life_Expectancy,
+SUM(CASE WHEN GDP <= 1500 THEN 1 ELSE 0 END) Low_GDP_Count,
+AVG(CASE WHEN GDP <= 1500 THEN `Life Expectancy` ELSE NULL END) Low_GDP_Life_Expectancy
+FROM world_life_expectancy
+;
+
+SELECT *
+FROM world_life_expectancy
+;
+
+
+SELECT Status, ROUND(AVG(`Life Expectancy`),1)
+FROM world_life_expectancy
+GROUP BY Status
+;
+
+
+SELECT Status, COUNT(DISTINCT Country), ROUND(AVG(`Life Expectancy`),1)
+FROM world_life_expectancy
+GROUP BY Status
+;
+
+SELECT Country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(BMI),1) AS BMI
+FROM world_life_expectancy
+GROUP BY Country
+HAVING Life_Exp > 0
+AND BMI > 0
+ORDER BY BMI ASC
+;
+
+SELECT Country, Year, `Life expectancy`, `Adult Mortality`, 
+SUM( `Adult Mortality`) OVER(PARTITION BY Country ORDER BY Year) AS Rolling_Total
+FROM world_life_expectancy
+WHERE Country LIKE 'United%'
+;
+
+
+
